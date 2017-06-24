a063ef7ca01e380fa40ba89699582565f9c27e87
MariPerez <mariperez259@gmail.com>
2017-06-24 12:28:55 -0400

    Agregando DAOModuo4

--- FinUcabWebService/src/java/BaseDatosDAO/Modulo4DAO.java
+++ FinUcabWebService/src/java/BaseDatosDAO/Modulo4DAO.java
@@ -5,10 +5,39 @@
  */
 package BaseDatosDAO;
 
+import BaseDatosDAO.Interfaces.IDAOCategoria;
+import Dominio.Entidad;
+import java.util.Dictionary;
+
 /**
  *
  * @author Junior
  */
-public class Modulo4DAO {
+public class Modulo4DAO extends DAO implements IDAOCategoria {
     
+    @Override
+    public int Agregar(Entidad e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Entidad Modificar(Entidad e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Entidad Consultar(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Dictionary<Integer, Entidad> ConsultarTodos() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    @Override
+    public String eliminarCategria(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+   
 }
+
