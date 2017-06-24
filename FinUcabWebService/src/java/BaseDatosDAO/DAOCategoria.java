4e9b352fbe23519ac25ea259a4693417ae593548
MariPerez <mariperez259@gmail.com>
2017-06-24 17:48:10 -0400

    Cambiando Nombre DAO

---
+++ FinUcabWebService/src/java/BaseDatosDAO/DAOCategoria.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BaseDatosDAO;
+
+import BaseDatosDAO.Interfaces.IDAOCategoria;
+import Dominio.Entidad;
+import java.util.Dictionary;
+
+/**
+ *
+ * @author MariPerez
+ */
+public class DAOCategoria extends DAO implements IDAOCategoria {
+    
+    @Override
+    public int Agregar(Entidad e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Entidad Modificar(Entidad e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Entidad Consultar(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Dictionary<Integer, Entidad> ConsultarTodos() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    @Override
+    public String eliminarCategria(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+   
+}
+
