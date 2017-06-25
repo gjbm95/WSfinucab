a1df808c464a65fc84c96045162c8cc889e12d1a
juanp22 <juan12413@gmail.com>
2017-06-25 14:18:59 -0400

    Prueba

--- FinUcabWebService/src/java/BaseDatosDAO/Modulo5DAO.java
+++ FinUcabWebService/src/java/BaseDatosDAO/Modulo5DAO.java
@@ -1,24 +1,4 @@
-<<<<<<< HEAD
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package BaseDatosDAO;
 
-/**
- *
- * @author Junior
- */
-public class Modulo5DAO {
-    
-}
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package BaseDatosDAO;
 
 import BaseDatosDAO.Interfaces.IDAOPago;
@@ -51,4 +31,4 @@ public class Modulo5DAO extends DAO implements IDAOPago{
     }
     
 }
->>>>>>> Develop
+
