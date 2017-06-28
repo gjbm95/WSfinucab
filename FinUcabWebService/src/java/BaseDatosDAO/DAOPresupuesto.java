343121883c1da7605d04f598d61c56e83c750e80
William lopez <wrlopez8@gmail.com>
2017-06-28 16:09:14 -0400

    borrando .class

--- FinUcabWebService/src/java/BaseDatosDAO/DAOPresupuesto.java
+++ FinUcabWebService/src/java/BaseDatosDAO/DAOPresupuesto.java
@@ -10,9 +10,18 @@ import Dominio.Entidad;
 import Dominio.Presupuesto;
 import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Dictionary;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
 
 /**
  *
@@ -73,6 +82,34 @@ public class DAOPresupuesto extends DAO implements IDAOPresupuesto {
     @Override
     public int eliminarPresupuesto(int id) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+      public JsonArray getUltimosPresupuestos(int id) {
+        CallableStatement cstm;
+        JsonArray array = null;
+        try {
+            Statement st = conn.createStatement();
+            cstm = conn.prepareCall("{ call obtenerUltimosPresupuestos(?)}");
+            cstm.setInt(1, id);
+            ResultSet rs = cstm.executeQuery();
+            JsonObjectBuilder cuentaBuilder = Json.createObjectBuilder();
+            JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+            int n = 0;
+            while (rs.next()) {
+                n++;
+                cuentaBuilder.add("est_id", "5." + Integer.toString(n));
+                cuentaBuilder.add("est_fecha", rs.getString("pr_fecha"));
+                cuentaBuilder.add("est_transaccion", rs.getString("pr_nombre"));
+                JsonObject cuentaJsonObject = cuentaBuilder.build();
+                arrayBuilder.add(cuentaJsonObject);
+            }
+            array = arrayBuilder.build();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(DaoTarjeta_Credito.class.getName()).log(Level.SEVERE, null, ex);
+
+        }
+        return array;
     }
     
 }
