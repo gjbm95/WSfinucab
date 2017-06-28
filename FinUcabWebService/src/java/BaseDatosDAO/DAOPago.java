8d2c90d36a4fdbf90dc3194423bd3d59a694dddf
juanp22 <juan12413@gmail.com>
2017-06-28 13:27:41 -0400

    acomodando

--- FinUcabWebService/src/java/BaseDatosDAO/DAOPago.java
+++ FinUcabWebService/src/java/BaseDatosDAO/DAOPago.java
@@ -26,34 +26,10 @@ public class DAOPago extends DAO implements IDAOPago{
 
     @Override
     public int agregar(Entidad e) {
-/*
-        Pago pago = (Pago) e;
-        int respuesta;
-        try {
-            Connection conn = Conexion.conectarADb();
-            Statement st = conn.createStatement();
-            CallableStatement pag = conn.prepareCall("{ call AgregarPago(?,?,?,?,?) }");
-            pag.setFloat(1, pago.getTotal());
-            pag.setString(2, pago.getDescripcion());
-            pag.setString(3, pago.getTipo());
-            pag.setInt(4, pago.getCategoria());
-            pag.setInt(5, pago.getIdUsario());
-            pag.executeQuery();
-                        
-            if (pag.execute()) {  respuesta = 1; }
-            else { respuesta = 0;  }
-            
-            st.close();
-        } catch (Exception ex) {
-
-            respuesta = 2;
-
-        }
-        return respuesta;
-*/
 
             Pago pago = (Pago) e;
             CallableStatement pag;
+            
         int idPago = 0;
         try {
             pag = conn.prepareCall("{ call AgregarPago(?,?,?,?,?) }");
@@ -76,9 +52,10 @@ public class DAOPago extends DAO implements IDAOPago{
 
     @Override
     public Entidad modificar(Entidad e) {
+        
         Pago pago = (Pago) e;
-     
         CallableStatement cstmt;
+        
         try {
             cstmt = conn.prepareCall("{ call ModificarPago(?,?,?,?,?,?) }");
             cstmt.setInt(1,pago.getIdPago());
