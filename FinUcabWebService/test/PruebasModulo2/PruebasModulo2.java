84c6a09c3240e232ecd39c69646372d91ac18407
Garry <garryjrbruno@hotmail.com>
2017-06-29 12:42:38 -0400

    Comenzando prueba

--- FinUcabWebService/test/PruebasModulo2/PruebasModulo2.java
+++ FinUcabWebService/test/PruebasModulo2/PruebasModulo2.java
@@ -5,6 +5,13 @@
  */
 package PruebasModulo2;
 
+import Dominio.Usuario;
+import Logica.Comando;
+import Logica.FabricaComando;
+import java.io.StringReader;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -37,9 +44,47 @@ public class PruebasModulo2 {
     public void tearDown() {
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+   
+     /** Prueba encargada de verificar el funcionamiento de actualizar
+     *   datos de cuentas de usuario. 
+     * @param decodifico String con estructura json
+     * @return JsonObject del string
+     */
+     @Test
+     public void actualizarCuentaTest() {
+         
+      String decodifico = "{ \"u_id\" : \"1\" , \"u_usuario\" : \"Eoeooeoe\" ,"
+              + " \"u_nombre\" : \"Alejandro\""
+      + ", \"u_apellido\" : \"Negrin\", \"u_correo\" :"
+              + " \"aledavid21@hotmail.com\", "
+      + "\"u_pregunta\" : \"Nombre de mi mama\" ,"
+              + " \"u_respuesta\" : \"/alejandra\", "
+               + "\"u_password\" : \"123456\" }";
+          JsonObject usuarioJSON = this.stringToJSON(decodifico);
+          Usuario usuario = new Usuario();
+          usuario.jsonToUsuario(usuarioJSON);
+            
+          Comando command = FabricaComando.
+                  instanciarComandoActualizarDatosUsuario(usuario);
+            command.ejecutar();
+            
+            
+     
+     
+     }
+     
+     
+     
+    /**
+     * Funcion que convierte un string con estructura JSON en JsonObject
+     *
+     * @param decodifico String con estructura json
+     * @return JsonObject del string
+     */
+    private JsonObject stringToJSON(String decodifico) {
+        JsonReader reader = Json.createReader(new StringReader(decodifico));
+        JsonObject jsonObj = reader.readObject();
+        reader.close();
+        return jsonObj;
+    }
 }
